88b1867ddd3d2866e7ce666ec51db909cae562ba
Andre Luige <Andre.Luige@nortal.com>
2023-05-27 10:12:31 +0300

    Changed default window size for windows

--- Part 1 - Displaying Data/MonkeyFinder/App.xaml.cs
+++ Part 1 - Displaying Data/MonkeyFinder/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace MonkeyFinder;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using WinRT.Interop;
+
+namespace MonkeyFinder;
 
 public partial class App : Application
 {
@@ -6,6 +11,19 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+#if WINDOWS
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(540, 1000));
+        });
+#endif
+
+        MainPage = new AppShell();
 	}
 }
